9fc9cf510fe19cb984473d3947f2eed724079d2b
GovindaEkbote <govindaekbote7@gmail.com>
2025-04-13 22:09:31 +0530

    feat: Dotenv

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,28 +1,23 @@
-
 module.exports = {
-    extends: ['@commitlint/cli','@commitlint/config-conventional'],
+    extends: ['@commitlint/cli', '@commitlint/config-conventional'],
     rules: {
-      'type-enum': [
-        2,
-        'always',
-        [
-          'feat',
-          'fix',
-          'docs',
-          'style',
-          'refactor',
-          'pref',   
-          'test',
-          'build',
-          'ci',
-          'chore',
-          'revert'
-        ]
-      ],
-      'subject-case': [2, 'always', 'sentence-case']
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'docs',
+                'style',
+                'refactor',
+                'pref',
+                'test',
+                'build',
+                'ci',
+                'chore',
+                'revert'
+            ]
+        ],
+        'subject-case': [2, 'always', 'sentence-case']
     }
-  };
-  
-
-
- 
+}
--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,9 @@
-// Enviroment setup
+const dotenvFlow = require('dotenv-flow')
+dotenvFlow.config()
+
+module.exports = {
+    ENV: process.env.ENV,
+    PORT: process.env.PORT,
+    SERVER_URL: process.env.SERVER_URL,
+    DATABASE_URL: process.env.DATABASE_URL
+}
